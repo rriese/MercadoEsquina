70d2f68c3398d0ec3433ef758fbe28fef5b4823b
rriese <rafael.riese@catolicasc.org.br>
2017-08-15 19:39:09 -0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MercadoEsquina.Startup))]
+namespace MercadoEsquina
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
